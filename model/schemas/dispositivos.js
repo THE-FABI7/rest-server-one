5213efe0756ed06e6c2ab1426356c56a57c851bd
THE FABI7 <fabianguancha13@gmail.com>
2023-08-19 14:59:29 -0600

    creacion de los schemas

---
+++ model/schemas/dispositivos.js
@@ -0,0 +1,37 @@
+// const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const validator = require("mongose-unique-validator");
+
+const dispostivosSchema = mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  tipo: {
+    type: String,
+    required: true,
+  },
+  marca: {
+    type: String,
+    required: true,
+  },
+  valor: {
+    type: Number,
+    required: true,
+  },
+  actualizacion: {
+    type: Date,
+    required: true,
+    min: new Date("2010-01-01"),
+    max: new Date("2023-08-19"),
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 100000,
+    max: 7000000,
+  },
+});
+
+dispostivosSchema.plugin(validator);
+module.exports = dispostivosSchema;
