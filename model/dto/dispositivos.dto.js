64b48555faf01aab56479adf4701458c7498cb55
Lenuard <daniel.1701823203@ucaldas.edu.co>
2023-08-19 16:32:28 -0500

    Creacion del dto

---
+++ model/dto/dispositivos.dto.js
@@ -0,0 +1,29 @@
+// packes
+const mongoose = require('mongoose');
+const db = require("../db-connection/mongodb")
+
+const schema = require("../schemas/dispositivos")
+
+db();
+/**Declares the data managemet functions */
+schema.statics = {
+    create: function (data, callback) {
+        let doc =  new this(data);
+        doc.save(callback);
+    },
+    getAll: function (query, callback) {
+         this.find(query, callback);
+    },
+    getByMarca: function (query, callback) {
+        this.find(query, callback);
+    },
+    update: function (query, data, callback) {
+        this.findOneAndUpdate(query,{$set: data}, {new: true}, callback);
+    },
+    delete: function (query, callback) {
+        this.findOneAndDelete(query);
+    }
+}
+
+const dto = mongoose.model('coll_phones', schema);
+module.exports = dto;
